71e2c11b01f997ed1dc827807b792a982eb0536a
Ilham <ilham.personal29@gmail.com>
2024-12-13 15:24:33 +0800

    add inventory service

--- app/Http/Domains/Inventories/Model/Inventory.php
+++ app/Http/Domains/Inventories/Model/Inventory.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Domains\Inventories\Model;
 
+use App\Http\Domains\Products\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'name',
+        'stock',
+        'location',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
